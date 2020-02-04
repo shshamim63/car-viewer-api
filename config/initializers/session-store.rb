04ec512532d57fd88124d740c04a58ed568da4b1
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-02-04 21:18:20 +0600

    :art: Moves data and configure session_store for tokens
    Moves codes from session_store to reck initializer
    Configure session_store initializer

--- config/initializers/session-store.rb
+++ config/initializers/session-store.rb
@@ -1,12 +1,1 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origin "http://localhost:3000"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    creadentials: true
-  end
-  allow do
-    origin "http://localhost:3000"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    creadentials: true
-  end
-end
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain:car-viewer-api.herokuapp.com
