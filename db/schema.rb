16b0812fd57c65a8253184fbd25f5eadf73102e2
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-02-05 22:37:03 +0600

    :art: Generates model for Appoinments
    Appoint model references Car and User

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_140753) do
+ActiveRecord::Schema.define(version: 2020_02_05_163552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.string "data"
+    t.string "time"
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["car_id"], name: "index_appointments_on_car_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "modelname"
@@ -35,4 +46,6 @@ ActiveRecord::Schema.define(version: 2020_02_05_140753) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "appointments", "cars"
+  add_foreign_key "appointments", "users"
 end
