34c0c60f90145078ebbc12d17b83686b06193c5d
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-02-05 22:15:59 +0600

    :art: Adds Car module
    Adds cars module with controllers.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_170757) do
+ActiveRecord::Schema.define(version: 2020_02_05_140753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "modelname"
+    t.integer "fee"
+    t.integer "payable"
+    t.integer "duration"
+    t.string "representative"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
