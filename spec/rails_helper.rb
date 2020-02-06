6d1ae2dceeadfe087431afd4d01e6fad5a2ba343
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-02-06 22:51:31 +0600

    :rotating_light: Disable a rule
    Disables rule that has been caused on rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,6 +22,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
+# rubocop:disable NonDeterministicRequireOrder
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
