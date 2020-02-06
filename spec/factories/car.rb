c2db2d9a7a97911a4d26072ffae0fbf5170b0d5c
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-02-06 15:42:27 +0600

    :white_check_mark: Adds test for car model
    Checks for cars model validaton
    Checks for modelname presence in the car model

---
+++ spec/factories/car.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :car do
+    sequence(:modelname) { |n| "MyString #{n}" }
+    fee { 29 }
+    payable { 295 }
+    duration { 2 }
+    representative { 'Seller' }
+    description {'Ducimus mollitia deserunt. Dicta et corporis.'}
+  end
+end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    it 'should test for a valid factory' do
+      expect(build(:car)).to be_valid
+    end
+    it 'should validate the presence of modelname' do
+      car = build(:car, modelname: '')
+      expect(car).not_to be_valid
+      expect(car.errors.messages[:modelname]).to include("can't be blank")
+    end
+  end
 end
