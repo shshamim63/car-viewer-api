bb660a8914557976937724bdc41eafe5844016d0
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-02-06 21:44:52 +0600

    :rotating_light: Updates Linter errors on Application controller
    Removes extra spacing, adds new line, adds single quote

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,7 @@
 class AppointmentsController < ApplicationController
   def index
     if @current_user
-      appointments = @current_user.appointments.order("created_at DESC").joins(:cars)
+      appointments = @current_user.appointments.order('created_at DESC').joins(:cars)
       if appointments
         render json: appointments
       else
@@ -16,14 +16,14 @@ class AppointmentsController < ApplicationController
 
   def create
     if @current_user
-      appointment = @current_user.appointments.build(car_id: params[:car_id], date: params[:date],                                                    time: params[:time], city: params[:city])
+      appointment = @current_user.appointments.build(car_id: params[:car_id], date: params[:date],   time: params[:time], city: params[:city])
       if appointment.save
         render json: {
           status: :created,
           appointment: appointment
         }
       else
-        render json: {status: 200}
+        render json: { status: 200 }
       end
     else
       render json: { logged_in: false }
