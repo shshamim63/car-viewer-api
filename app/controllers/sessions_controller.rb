aff48458072756b35337b1321bbf7f1b7d0f6b61
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-02-13 20:30:01 +0600

    :bug: Fixing bug caused by session
    Removes default serializer declared as nill

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
         user: user
       }
     else
-      render json: { status: 401 }
+      render json: { status: 401 }, serializer: nil
     end
   end
 
@@ -28,6 +28,7 @@ class SessionsController < ApplicationController
   end
 
   def logout
+    puts session
     reset_session
     render json: {
       status: 200,
