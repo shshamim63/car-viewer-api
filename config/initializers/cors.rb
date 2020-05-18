f33217a058c2d59b90078c6bdceda7e4e95dbdf4
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-05-19 05:22:18 +0600

    :art: Changing base url
    Refactors base url declaration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'car-viewer.netlify.com'
+    origins 'http://localhost:3000', 'https://car-viewer.netlify.com'
 
     resource '*',
     headers: :any,
