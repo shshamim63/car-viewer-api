b507a6e76311454ca1a0325a146af5125db9bda0
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-05-19 03:16:20 +0600

    :recycle: Refactors cors.rb file
    Deletes duplicate declaration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
-
-    resource '*',
-    headers: :any,
-    methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    credentials: true
-  end
-  
-  allow do
-    origins 'https://car-viewer.netlify.com'
+    origins 'localhost:3000', 'car-viewer.netlify.com'
 
     resource '*',
     headers: :any,
